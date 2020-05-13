5c7799872ddd452265648b4a41101d53d1416bef
allezz <aerbay@gmail.com>
2020-05-13 18:34:51 +0200

    Add new test examples,Create/migrate database

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,5 +1,8 @@
-class Api::V1::SearchesController < ApplicationController
-  def create
+# frozen_string_literal: true
 
-  end
+class Api::V1::SearchesController < ApplicationController
+  def index
+    api_key = Rails.application.credentials.movie_db[:api_key]
+    response = RestClient.get("https://api.themoviedb.org/3/search/multi?api_key=#{api_key}&language=en-US&query=Will%20Smith&page=1&include_adult=false")
+   end
 end
--- spec/requests/api/v1/api_provides_search_functionality_spec.rb
+++ spec/requests/api/v1/api_provides_search_functionality_spec.rb
@@ -9,16 +9,40 @@ RSpec.describe Api::V1::SearchesController, type: :request do
           }
     end
 
-    it 'successfully returns name' do
-      expect(JSON.parse(response.body)['name']).to eq 'Will Smith'
-    end
-
     it 'should return a 200 response' do
       expect(response).to have_http_status 200
     end
 
+    it 'successfully returns first results name' do
+      expect(JSON.parse(response.body)['result'][0]['name']).to eq 'Will Smith'
+    end
+
     it 'successfully returns related movie title' do
-      expect(JSON.parse(response.body)['title']).to eq 'Suicide Squad'
+      expect(JSON.parse(response.body)['result'][0]['known_for'][0]['original_title']).to eq 'Suicide Squad'
+    end
+
+    it 'successfully returns first results name' do
+      expect(JSON.parse(response.body)['result'][12]['name']).to eq 'Willow Smith'
+    end
+
+    it 'successfully returns related movie title' do
+      expect(JSON.parse(response.body)['result'][12]['known_for'][0]['original_title']).to eq 'I Am Legend'
+    end
+
+    it 'successfully returns first results name' do
+      expect(JSON.parse(response.body)['result'][17]['name']).to eq 'Wilson Smith'
+    end
+
+    it 'successfully returns related movie title' do
+      expect(JSON.parse(response.body)['result'][17]['known_for'][1]['original_title']).to eq 'Krisha'
+    end
+
+    it 'successfully returns first results name' do
+      expect(JSON.parse(response.body)['result'][19]['name']).to eq 'Willie Smith III'
+    end
+
+    it 'successfully returns related movie title' do
+      expect(JSON.parse(response.body)['result'][19]['known_for'][0]['original_title']).to eq 'Jesus Christ Superstar Live in Concert'
     end
   end
 end
