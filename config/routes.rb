6a8f2201458f3e4bfbe24e9dbbd4b050015331f7
japh <jenny.scherr@svenskaspel.se>
2020-05-15 14:38:16 +0200

    added facebook controller, updated the user model, updated the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'api/v1/auth', controllers: {
+    registrations: :facebook_login
+  }
   namespace :api do
     namespace :v1 do
       resources :movie_person, only: :show
