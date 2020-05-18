8bdd24e0d54332cb2d10b0ec396a6188dc3f82b7
Erik Björn <erik.g.bjoern@gmail.com>
2020-05-18 15:58:11 +0200

    configuration

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -5,7 +5,7 @@ DeviseTokenAuth.setup do |config|
   # client is responsible for keeping track of the changing tokens. Change
   # this to false to prevent the Authorization header from changing after
   # each request.
-  # config.change_headers_on_each_request = true
+  config.change_headers_on_each_request = false
 
   # By default, users will need to re-authenticate after 2 weeks. This setting
   # determines how long tokens will remain valid after they are issued.
@@ -42,7 +42,7 @@ DeviseTokenAuth.setup do |config|
   # config.default_callbacks = true
 
   # Makes it possible to change the headers names
-  # config.headers_names = {:'access-token' => 'access-token',
+  #config.headers_names = {:'access-token' => 'access-token',
   #                        :'client' => 'client',
   #                        :'expiry' => 'expiry',
   #                        :'uid' => 'uid',
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'api/v1/auth', controllers: {
+  mount_devise_token_auth_for 'User', at: '/api/v1/auth', controllers: {
     registrations: :facebook_login
   }
   namespace :api do
